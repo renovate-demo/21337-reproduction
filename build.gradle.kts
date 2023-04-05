12a6a678eb1eea5b6dde514fc4084a95b01eef51
KordianD <KordianD@users.noreply.github.com>
2023-04-05 13:03:02 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.springframework.boot") version ("2.7.8")
+    id("io.spring.dependency-management") version ("1.0.13.RELEASE")
+    id("com.github.ben-manes.versions") version ("0.45.0")
+    id("com.diffplug.spotless") version ("6.14.0")
+    id("org.jetbrains.kotlin.jvm") version ("1.7.20")
+    id("org.jetbrains.kotlin.plugin.spring") version ("1.7.20")
+}
